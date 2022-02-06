69a75de4d9aa8b359d0bc37b1ae65ae1ebc173d2
ifui <ifui@foxmail.com>
2022-02-06 20:15:48 +0800

    fix: 修复图片下载没反应的问题：缺少 pillow 库

--- robot/manage.py
+++ robot/manage.py
@@ -2,6 +2,6 @@
 from scrapy import cmdline
 
 cmdline.execute(
-    'scrapy crawl 81cn_section -a start_time=20220126 -a end_time=20220126'.split())
+    'scrapy crawl 81cn_section -a start_time=20220127 -a end_time=20220127'.split())
 # cmdline.execute(
 #     'scrapy crawl 81cn -a startTime=20220111 -a endTime=20220112'.split())
